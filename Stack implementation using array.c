138759f4fa73fc8474b49b86a34f17f61f96a5dd
Mohammad Sujon <62953491+mdsujon428@users.noreply.github.com>
2020-04-13 16:03:02 +0000

    complete

---
+++ Stack implementation using array.c
@@ -0,0 +1,98 @@
+#include<stdio.h>
+
+#include<conio.h>
+
+
+int stack[100],choice,n,i,x,top;
+ 
+ void push(void)
+{
+ if(top>=n-1)
+{
+  printf("\n STACK is Overfloe");
+}
+ 
+else 
+
+{ printf("\n\n Enter a Value to be pushed:");
+ scanf("%d",&x);
+ stack[top]=x;
+}
+}
+ 
+ void pop(void)
+
+{
+if(top>=0)
+{
+ printf("\n\n poped element is=%d",stack[top]);
+  top=top-1;
+}
+else{
+ print("\n\n STACK is Emty");
+}
+}
+void display(void)
+
+{
+if(top>=0)
+{
+for(i=top;i>=0;i--)
+{
+printf("\n\n STACK Element is:%d",stack[top]);
+}
+
+}
+
+  printf("\n\nPlease Press another Choice ");
+
+}
+int main()
+
+{top=-1;
+
+printf("\n Enter the size of STACK[100]:");
+
+scanf("%d",&n);
+printf("\n\n\n 1.Push\n 2.Pop \n 3.Display\n");
+
+
+
+do
+{
+ printf("\n\n Enter your choice: ");
+ scanf("%d",&choice);
+
+ switch (choice)
+  
+{
+ case 1:{
+  push();
+  break;
+}
+ case 2:
+{
+ pop();
+ break;
+}
+ case 3:
+{
+ display();
+  break;
+}
+ case 4:
+{
+printf("\n EXIT OPERATION \n");
+ break;
+}
+ default :
+
+{
+ printf("\n\n Enter choice, 1 to 4");
+}
+
+}
+}
+while(choice!=4)
+return 0;
+}
