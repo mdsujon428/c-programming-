7e58b5cb9d87e05d1dce2973590e13f4b248b32e
Mohammad Sujon <62953491+mdsujon428@users.noreply.github.com>
2020-04-20 17:50:24 +0600

    From URI online jude
    Read three values (variables A, B and C), which are the three student's grades. Then, calculate the average, considering that grade A has weight 2, grade B has weight 3 and the grade C has weight 5. Consider that each grade can go from 0 to 10.0, always with one decimal place.
    
    Input
    The input file contains 3 values of floating points with one digit after the decimal point.
    
    Output
    Print MEDIA(average in Portuguese) according to the following example, with a blank space before and after the equal signal.

---
+++ Average 2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{ double A,B,C,AVG;
+scanf("%lf",&A);
+scanf("%lf",&B);
+scanf("%lf",&C);
+AVG=((A*2)+(B*3)+(C*5))/(2+3+5);
+printf("MEDIA = %.1lf\n",AVG);
+    
+return 0;
+}
